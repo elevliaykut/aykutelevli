05c890357decf00e671cd94f6ea56f58a9450651
elevliaykut <elevli.aykut.28@gmail.com>
2020-01-30 14:13:03 +0300

    File Structure

--- src/components/header.js
+++ src/components/header.js
@@ -16,17 +16,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+z
     </div>
   </header>
 )
---
+++ src/components/personal-card/personal-card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalCard = () => {
+    return(
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
+}
+
+export default PersonalCard
