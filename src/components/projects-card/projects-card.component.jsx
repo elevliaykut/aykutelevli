049c099e0088924c624e1caaefb1035d376d6e50
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-01 00:42:42 +0300

    Refactoring & Edit Projects Card

--- src/components/projects-card/projects-card.component.jsx
+++ src/components/projects-card/projects-card.component.jsx
@@ -17,12 +17,15 @@ const ProjectsCard = () => {
             <SubTitle>Geçmiş</SubTitle>
             <ItemList>
                 <Item>
-                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Bir film sitesinin dışarıya verdiği API' yı kullanarak güncel filmleri gösterdiğim basit bir React Native projesi yaptım.
+                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Bir film sitesinin dışarıya verdiği API' yı kullanarak güncel filmleri gösterdiğim basit bir <CustomLink targetUrl="https://github.com/elevliaykut/movie-list"> Film Uygulaması </CustomLink> yaptım. <Date> (2020) </Date>
                 </Item>
                 <Item>
-                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Bitirme Projesi olarak geliştirdiğimiz <CustomLink targetUrl="https://github.com/elevliaykut/CareerProjectFrontEnd">Kariyer Planlama Platformu</CustomLink> 2 yakın arkadaşım ile
+                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> İzmir lokasyonunda bulunan <CustomLink targetUrl="https://bro35.com/"> Bro35 </CustomLink> şirketinde <CustomLink targetUrl="https://emlaksitem.com/"> Emlaksitem </CustomLink> projesinde FrontEnd Developer olarak görev aldım. <Date> (2019) </Date>
+                </Item>
+                <Item>
+                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji><CustomLink targetUrl="https://github.com/elevliaykut/CareerProjectFrontEnd"> Kariyer Planlama Platformu</CustomLink>' nu 2 yakın arkadaşım ile
                     beraber geliştirdik. Diğer Kariyer Planlama platformlarından farklı olarak sınav modülü entegre ettik. Böylece Insan Kaynaklarında görev alan
-                    kişiler daha az efor harcayarak ilgili pozisyona uygun adayı bulabiliyorlar. <Date> (2019 - CoFounder) </Date>
+                    kişiler daha az efor harcayarak ilgili pozisyonlara uygun adayları bulabiliyorlar. <Date> (2019 - CoFounder) </Date>
                 </Item>
                 <Item>
                     <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Docker Sanallaştırma teknolojisini kullanarak <BoldText>Mysql</BoldText> ve <BoldText>Nginx</BoldText>' i farklı containerlarda
@@ -33,7 +36,7 @@ const ProjectsCard = () => {
                     bir <CustomLink targetUrl="https://github.com/elevliaykut/Dockerizing-Laravel-App-On-Ubuntu"> Akademik Dergi Yönetim Sistemi </CustomLink> geliştirdim. <Date> (2018) </Date>
                 </Item>
                 <Item>
-                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Plotların kolayca rezervasyon yapıp uçuş için simulator kiralayabileceği bir <CustomLink targetUrl="https://github.com/elevliaykut/Simulator-Management-System">Simulator Yönetim Sistemi</CustomLink> geliştirdim.<Date> (2017) </Date>
+                    <CustomEmoji label='past' emoji='✔️'></CustomEmoji> Pilotların kolayca rezervasyon yapıp uçuş için simulator kiralayabileceği bir <CustomLink targetUrl="https://github.com/elevliaykut/Simulator-Management-System">Simulator Yönetim Sistemi</CustomLink> geliştirdim.<Date> (2017) </Date>
                 </Item>
                 
             </ItemList>
