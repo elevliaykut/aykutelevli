828e9c8d6523092ef8dd3774f489bd3515bf5224
elevliaykut <elevli.aykut.28@gmail.com>
2020-01-31 22:40:15 +0300

    Projects Edit

--- src/components/projects-card/projects-card.component.jsx
+++ src/components/projects-card/projects-card.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CollectiveTitle, SubTitle, ItemList, Item, BoldText } from './../collective-card/collective-card.styles'
+import { CollectiveTitle, SubTitle, ItemList, Item, BoldText, Date } from './../collective-card/collective-card.styles'
 import CustomEmoji from './../custom-emoji/custom-emoji.component'
 import CustomLink from './../custom-link/custom-link.component'
 
@@ -10,11 +10,18 @@ const ProjectsCard = () => {
             <SubTitle>Mevcut</SubTitle>
             <ItemList>
                 <Item>
-                    <CustomEmoji label='present' emoji='🎧'></CustomEmoji> Şuan da <CustomLink targetUrl='https://github.com/elevliaykut/WhoIsNext'>Win</CustomLink>,
-                    mobil uygulamam üzerinde geliştirme yaparken <BoldText>React Native</BoldText>, <BoldText>Node.js</BoldText>, <BoldText> Javascirpt </BoldText> yeteneklerimi geliştirmiş oluyorum.
+                    <CustomEmoji label='present' emoji='🎧'></CustomEmoji> Şuan da <CustomLink targetUrl='https://github.com/elevliaykut/Whoisnext'>Win</CustomLink>,
+                    mobil uygulamam üzerinde geliştirme yaparken <BoldText>React Native</BoldText>, <BoldText>Node.js</BoldText>, <BoldText> Javascirpt </BoldText> yeteneklerimi geliştirmiş oluyorum.<Date> (2020) </Date>
                 </Item>
             </ItemList>
             <SubTitle>Geçmiş</SubTitle>
+            <ItemList>
+                <Item>
+                    <CustomEmoji label='present' emoji='✔️'></CustomEmoji>Bitirme Projesi olarak geliştirdiğimiz <CustomLink targetUrl="https://github.com/elevliaykut/CareerProjectFrontEnd">Kariyer Planlama Platformu</CustomLink> 3 yakın arkadaşım ile
+                    beraber geliştirdik. Diğer Kariyer Planlama platformlarından farklı olarak sınav modülü entegre ettik. Böylece Insan Kaynaklarında görev alan
+                    kişiler daha az efor harcayarak ilgili pozisyona uygun adayı bulabiliyorlar. <Date> (2019) </Date>
+                </Item>
+            </ItemList>
         </>
     )
 }
