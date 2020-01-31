72559d3140f810e0c8175acdb4b7d24c7c9661e6
elevliaykut <elevli.aykut.28@gmail.com>
2020-01-31 21:38:18 +0300

    Project design

--- src/components/projects-card/projects-card.component.jsx
+++ src/components/projects-card/projects-card.component.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { CollectiveTitle } from './../collective-card/collective-card.styles'
+import { CollectiveTitle, SubTitle, ItemList, Item, BoldText } from './../collective-card/collective-card.styles'
 import CustomEmoji from './../custom-emoji/custom-emoji.component'
+import CustomLink from './../custom-link/custom-link.component'
+
 const ProjectsCard = () => {
-    return(
+    return (
         <>
             <CollectiveTitle><CustomEmoji label='Projects' emoji='👩🏻‍💻'></CustomEmoji> Projeler </CollectiveTitle>
+            <SubTitle>Mevcut</SubTitle>
+            <ItemList>
+                <Item>
+                    <CustomEmoji label='present' emoji='🎧'></CustomEmoji> Şuan da <CustomLink targetUrl='https://github.com/elevliaykut/WhoIsNext'>Win</CustomLink>,
+                    mobil uygulamam üzerinde geliştirme yaparken <BoldText>React Native</BoldText>, <BoldText>Node.js</BoldText>, <BoldText> Javascirpt </BoldText> yeteneklerimi geliştirmiş oluyorum.
+                </Item>
+            </ItemList>
+            <SubTitle>Geçmiş</SubTitle>
         </>
     )
 }
