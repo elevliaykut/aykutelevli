fa639d235d15d7e13275092e67551e9e1cd881dc
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-02 01:08:24 +0300

    Deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aykut Elevli`,
+    description: `Sth about my developer life`,
+    author: `@Aykut`,
+    pathPrefix: "/mysite",
+    
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
