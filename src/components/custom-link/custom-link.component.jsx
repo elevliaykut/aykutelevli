029cd3cb3141a63ee8c4e011a6a33cbce373b43f
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-02 14:30:52 +0300

    Edit

--- src/components/custom-link/custom-link.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const CustomLink = ({ targetUrl, children }) => (
-    <strong><a href={targetUrl} target='_blank' rel='noopener noreferrer'>{children}</a></strong>
-)
-
-export default CustomLink
--- src/components/footer/footer.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const FooterDiv = styled.div`
-    font-size:15px;
-    border-top:4px solid #eee;
-    margin-top:60px;
-    padding-top:20px;
-    text-align:right;
-`
