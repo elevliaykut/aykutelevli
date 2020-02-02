a29a42ec9b7002e96775a03770ae532178924ee0
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-02 14:28:57 +0300

    Refactored

---
+++ src/components/personal-card/personal-card.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { TopTitle,Summary,SocialIcons } from './../collective-card/collective-card.styles'
+import  CustomIcon  from './../customicon/custom-icon.component'
+
+import  LinkedinLogo  from './../../images/social-icons/linkedin.png'
+import  GithubLogo  from './../../images/social-icons/github.png'
+
+const PersonalCard = () => {
+    return (
+        <>
+            <TopTitle>Merhaba, Ben Aykut</TopTitle>
+            <Summary>
+                Kod yazmak ve bir uygulamayı hayata geçirene kadar geçen süre benim en büyük tutkum.Uygulama geliştirme yolculuğumda
+                güncel teknolojileri araştırarak ve bunları geliştirdiğim uygulamalarda kullanarak ilerliyorum.
+            </Summary>
+            <SocialIcons>
+                <CustomIcon
+                    targetUrl='https://www.linkedin.com/in/aykut-elevli/'
+					imageSource={LinkedinLogo}
+					altDescription='linkedin' />
+                <CustomIcon
+                    targetUrl='https://github.com/elevliaykut'
+					imageSource={GithubLogo}
+					altDescription='github' />
+            </SocialIcons>
+        </>
+    )
+}
+
+export default PersonalCard
---
+++ src/components/technologies-card/technology-card.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { CollectiveTitle } from './../collective-card/collective-card.styles'
+import { StackList, Item, TechOne, TechTwo, TechThree, TechFour, TechFive, TechSix, TechSeven, TechEight, BoldText } from './../collective-card/collective-card.styles'
+import CustomEmoji from './../custom-emoji/custom-emoji.component'
+const TechnologyCard = () => {
+    return (
+        <>
+            <CollectiveTitle><CustomEmoji label='tool' emoji='🛠'></CustomEmoji> Teknolojiler </CollectiveTitle>
+            <StackList>
+                <Item>
+                    Şuanda aktif olarak <TechOne><BoldText>React</BoldText></TechOne>, <TechTwo><BoldText>React Native</BoldText></TechTwo>, <TechThree><BoldText>Firebase</BoldText></TechThree> teknolojileri üzerinde
+                    çalışmalar yapıp, uygulamalar geliştirmekteyim.
+                </Item>
+                <Item>
+                    Genel olarak kullandığım teknolojiler <TechSix><BoldText>PHP</BoldText></TechSix>, <TechFive><BoldText>MySql</BoldText></TechFive>, <TechSeven><BoldText>Html</BoldText></TechSeven>, <TechEight><BoldText>Css</BoldText></TechEight> şekklinde devam ediyor.
+                </Item>
+                <Item>
+                    Sanallaştırma ve Uygulamaların daha kolay deploy süreçleri için <TechFour><BoldText>Docker</BoldText></TechFour> teknolojisini kullanıyorum.
+                </Item>
+                <Item>
+                    <TechOne><BoldText>Git</BoldText></TechOne>, <TechTwo><BoldText>Git Terminal</BoldText></TechTwo> 'i neredeyse hergün kullanıyorum. 
+                </Item>
+                <Item>
+                    Geliştirdiğim Projeler de <TechEight><BoldText>Unit Test</BoldText></TechEight>' leri gerçekleştiriyorum.
+                </Item>
+            </StackList>
+        </>
+
+    )
+}
+export default TechnologyCard
