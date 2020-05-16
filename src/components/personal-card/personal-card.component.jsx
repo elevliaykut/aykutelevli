a8c26ac1f8e2b096c53ab24e78220b57a4482019
elevliaykut <elevli.aykut.28@gmail.com>
2020-05-16 23:25:54 +0300

    header icon added

--- src/components/personal-card/personal-card.component.jsx
+++ src/components/personal-card/personal-card.component.jsx
@@ -20,7 +20,7 @@ const PersonalCard = () => {
             <MainContainer>
                 <div>
                     <TopTitle>Merhaba, Ben Aykut.</TopTitle>
-                    <Snippet>Full Stack Developer @ Kamion</Snippet>
+                    <Snippet>Full Stack Developer @ Kamion 💻</Snippet>
                     <Summary>
                         Kod yazmak ve bir uygulamayı hayata geçirene kadar geçen süre benim en büyük tutkum.Uygulama geliştirme yolculuğumda
                         güncel teknolojileri araştırarak ve bunları geliştirdiğim uygulamalarda kullanarak ilerliyorum.
