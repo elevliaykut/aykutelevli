291d007401197bfb837777436a6592aa630fcfcd
elevliaykut <elevli.aykut.28@gmail.com>
2020-05-16 23:01:30 +0300

    profile picture addded

--- src/components/personal-card/personal-card.component.jsx
+++ src/components/personal-card/personal-card.component.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
-import { TopTitle,Summary,SocialIcons,Snippet} from './../collective-card/collective-card.styles'
-import  CustomIcon  from './../customicon/custom-icon.component'
+import { TopTitle, Summary, SocialIcons, Snippet } from './../collective-card/collective-card.styles'
+import CustomIcon from './../customicon/custom-icon.component'
 
-import  LinkedinLogo  from './../../images/social-icons/linkedin.png'
-import  GithubLogo  from './../../images/social-icons/github.png'
-import  MediumLogo  from './../../images/social-icons/medium.png'
+import LinkedinLogo from './../../images/social-icons/linkedin.png'
+import GithubLogo from './../../images/social-icons/github.png'
+import MediumLogo from './../../images/social-icons/medium.png'
+import ProfilePicture from '../../images/profile.png'
 
 const PersonalCard = () => {
     return (
         <>
-            <TopTitle>Merhaba, Ben Aykut.</TopTitle>
-            <Snippet>Full Stack Developer @ Kamion</Snippet>
-            <Summary>
-                Kod yazmak ve bir uygulamayı hayata geçirene kadar geçen süre benim en büyük tutkum.Uygulama geliştirme yolculuğumda
-                güncel teknolojileri araştırarak ve bunları geliştirdiğim uygulamalarda kullanarak ilerliyorum.
+            <MainContainer>
+                <div>
+                    <TopTitle>Merhaba, Ben Aykut.</TopTitle>
+                    <Snippet>Full Stack Developer @ Kamion</Snippet>
+                    <Summary>
+                        Kod yazmak ve bir uygulamayı hayata geçirene kadar geçen süre benim en büyük tutkum.Uygulama geliştirme yolculuğumda
+                        güncel teknolojileri araştırarak ve bunları geliştirdiğim uygulamalarda kullanarak ilerliyorum.
             </Summary>
-            <SocialIcons>
-                <CustomIcon
-                    targetUrl='https://www.linkedin.com/in/aykut-elevli/'
-					imageSource={LinkedinLogo}
-					altDescription='linkedin' />
-                <CustomIcon
-                    targetUrl='https://github.com/elevliaykut'
-					imageSource={GithubLogo}
-					altDescription='github' />
-                <CustomIcon 
-                    targetUrl='https://medium.com/@elevli.aykut.28'
-                    imageSource={MediumLogo}
-                    altDescription='medium'/>
+                    <SocialIcons>
+                        <CustomIcon
+                            targetUrl='https://www.linkedin.com/in/aykut-elevli/'
+                            imageSource={LinkedinLogo}
+                            altDescription='linkedin' />
+                        <CustomIcon
+                            targetUrl='https://github.com/elevliaykut'
+                            imageSource={GithubLogo}
+                            altDescription='github' />
+                        <CustomIcon
+                            targetUrl='https://medium.com/@elevli.aykut.28'
+                            imageSource={MediumLogo}
+                            altDescription='medium' />
 
-            </SocialIcons>
+                    </SocialIcons>
+                </div>
+                <ImageContainer>
+                    <img src={ProfilePicture} alt='profile' />
+                </ImageContainer>
+            </MainContainer>
         </>
     )
 }
