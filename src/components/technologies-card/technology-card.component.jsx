32f46e3b79e514b0e48e314ba893863c17c92a65
elevli.aykut <elevli.aykut.28@gmail.com>
2020-07-14 23:56:31 +0300

    technology card refactored

--- src/components/technologies-card/technology-card.component.jsx
+++ src/components/technologies-card/technology-card.component.jsx
@@ -23,14 +23,14 @@ const TechnologyCard = () => {
             <CollectiveTitle><CustomEmoji label='tool' emoji='🛠'></CustomEmoji> Teknolojiler </CollectiveTitle>
             <StackList>
                 <Item>
-                    Şuan için tamamen <TechOne style={{ borderBottom: '3px solid #f0db4f' }}>JavaScript</TechOne> - <BoldText>React</BoldText>, <TechTwo style={{ borderBottom: '3px solid #0db7ed' }}>Node.js</TechTwo> - <BoldText>Express</BoldText>, <TechThree style={{ borderBottom: '3px solid #474a8a' }}>GraphQl</TechThree> teknolojileri üzerinde
+                    Şuan için tamamen <TechOne style={{ borderBottom: '3px solid #f0db4f' }}>JavaScript</TechOne> - <BoldText>React</BoldText>, <TechThree style={{ borderBottom: '3px solid #474a8a' }}>GraphQl</TechThree> teknolojileri üzerinde
                     odaklanıyorum.
                 </Item>
                 <Item>
                     Genel olarak kullandığım teknolojiler <TechSix style={{ borderBottom: '3px solid #ff9900' }}>PHP</TechSix>, <TechFive style={{ borderBottom: '3px solid #83ba63' }}>MySql</TechFive>, <TechSeven style={{ borderBottom: '3px solid #d24939' }}>Html</TechSeven>, <TechEight style={{ borderBottom: '3px solid #fd9426' }}>Css</TechEight> şekklinde devam ediyor.
                 </Item>
                 <Item>
-                    Cross Platform mobil uygulama geliştirirken <TechOne style={{ borderBottom: '3px solid #306998' }}>React Native</TechOne>, <TechTwo style={{ borderBottom: '3px solid #0db7ed' }}>Firebase</TechTwo> kullanıyorum.
+                    Cross Platform mobil uygulama geliştirmek için <TechOne style={{ borderBottom: '3px solid #306998' }}>React Native</TechOne> kullanıyorum.
                 </Item>
                 <Item>
                     <TechOne style={{ borderBottom: '3px solid #0db7ed' }}>Git</TechOne>, <TechTwo style={{ borderBottom: '3px solid #ff9900' }}>Git Terminal</TechTwo> 'i neredeyse hergün kullanıyorum.
@@ -39,7 +39,7 @@ const TechnologyCard = () => {
                     Temel seviye de <TechOne style={{ borderBottom: '3px solid #0db7ed' }}>Linux</TechOne> - <BoldText>Sistem</BoldText> bilgim var.
                 </Item>
                 <Item>
-                    Basit seviye de temel olarak <TechThree style={{ borderBottom: '3px solid #306998' }}>Jenkins</TechThree> - <BoldText> CI/CD </BoldText> süreçleri hakkında bilgi sahibiyim.
+                    Basit seviye de <TechThree style={{ borderBottom: '3px solid #306998' }}>Jenkins</TechThree> - <BoldText> CI/CD </BoldText> süreçleri hakkında bilgi sahibiyim.
                 </Item>
             </StackList>
             <CurrentLearning>
