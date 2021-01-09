164a9412a8c38283678d465473e00ad5431d3ea2
elevli.aykut <elevli.aykut.28@gmail.com>
2021-01-09 13:56:53 +0300

    title REFACTORED.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import PersonalCard from './../components/personal-card/personal-card.component'
 import PersonalityCard from './../components/personality-card/personality-card.component'
 import JourneyCard from './../components/journey-card/journey-card.component'
 import TechnologyCard from './../components/technologies-card/technology-card.component'
 import ProjectsCard from './../components/projects-card/projects-card.component'
 import Footer from './../components/footer/custom-footer.component'
-
+import { Helmet } from "react-helmet";
 const IndexPage = () => (
   <Layout>
     <div>
-      <PersonalCard />
-      <PersonalityCard />
-      <JourneyCard />
-      <TechnologyCard />
-      <ProjectsCard/>
-      <Footer/>
+        <Helmet>
+            <title>Aykut Elevli</title>
+        </Helmet>
+          <PersonalCard />
+          <PersonalityCard />
+          <JourneyCard />
+          <TechnologyCard />
+          <ProjectsCard/>
+          <Footer/>
     </div>
   </Layout>
 )
